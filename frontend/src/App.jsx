baf90f5dcdd3df54571505b89101be9acf435c2c
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-05-28 15:41:17 +0200

    Nav bar + hero video added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
-export const App = () => {
+import { BrowserRouter as Router } from "react-router-dom";
+import { Nav } from "./components/Header/Nav";
 
+export const App = () => {
   return (
-    <>
-      <h1>Welcome to Final Project!</h1>
-    </>
+    <Router>
+      <>
+        <Nav />
+      </>
+    </Router>
   );
 };
---
+++ frontend/src/components/Header/Nav.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
+import heroVideo from "../../assets/herovideo.mp4";
+
+// Styling for the navigation container
+const NavContainer = styled.nav`
+  padding: 5px;
+  margin: 0;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  background-color: #ffffff;
+  color: #000000;
+  font-family: "Apercu", sans-serif;
+  font-size: 16px;
+  display: flex;
+`;
+
+// Styling for the left side of the navigation bar
+const NavLeft = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+// Styling for the unordered list in the navigation bar
+const NavList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+`;
+
+// Styling for each item in the navigation bar
+const NavItem = styled.li`
+  padding: 10px;
+`;
+
+// Styling for the navigation links
+const NavLink = styled(Link)`
+  text-decoration: none;
+  padding: 10px 20px;
+  transition: color 0.3s ease;
+  color: #000000;
+
+  &:hover {
+    background-color: #f8f4f4;
+    border-radius: 20px;
+  }
+`;
+
+// Styling for the logo in the navigation bar
+const Logo = styled.div`
+  flex: 0;
+
+  img {
+    width: 250px;
+    height: auto;
+  }
+`;
+
+// Styling for the right side of the navigation bar
+const NavRight = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  flex: 1;
+  justify-content: center;
+`;
+
+// Styling for the buttons in the navigation bar
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #48544c;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+// Styling for the container of the hero video
+const HeroVideoContainer = styled.div`
+  position: fixed;
+  top: 85px;
+  left: 0;
+  width: 100%;
+  height: 80vh;
+  overflow: hidden;
+`;
+
+// Styling for the hero video itself
+const HeroVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+// Styling for the container of the text overlay
+const TextOverlayContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1001;
+  text-align: center;
+`;
+
+// Styling for the text overlay
+const TextOverlay = styled.div`
+  color: #ffffff;
+  font-size: 60px;
+  font-weight: bold;
+  line-height: 0.9;
+  animation: slideDown 1.5s ease-in-out;
+
+  @keyframes slideDown {
+    0% {
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
+export const Nav = () => {
+  return (
+    <>
+      <NavContainer>
+        <NavLeft>
+          <NavList>
+            <NavItem>
+              <NavLink to="/conference">Conference & Event</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/spa">Spa</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/dining">Dining</NavLink>
+            </NavItem>
+          </NavList>
+        </NavLeft>
+        <Logo>
+          <img src={logo} alt="Logo" />
+        </Logo>
+        <NavRight>
+          <Button>Book</Button>
+          <Button>Log In</Button>
+        </NavRight>
+      </NavContainer>
+      <HeroVideoContainer>
+        <HeroVideo autoPlay loop muted>
+          <source src={heroVideo} type="video/mp4" />
+        </HeroVideo>
+      </HeroVideoContainer>
+      <TextOverlayContainer>
+        <TextOverlay>
+          WELCOME TO <br></br>THE SUNSIDE HOTEL
+        </TextOverlay>
+      </TextOverlayContainer>
+    </>
+  );
+};

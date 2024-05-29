d63cd6dc0c4a595d6e90cc141a0ae517d4b3c801
Your Name <you@example.com>
2024-05-29 19:17:07 +0200

    add some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Footer } from "./components/Footer/Footer";
+import { MeetingSection } from "./components/MeetingSection/MeetingSection";
  
 
 export const App = () => {
@@ -6,6 +7,7 @@ export const App = () => {
   return (
     <>
       <h1>Welcome to Final Project!</h1>
+      <MeetingSection />
       <Footer />
     </>
   );
---
+++ frontend/src/components/MeetingSection/MeetingSection.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const MeetingSection = () => {
+  return (
+    <div>
+      <div>
+        <h2>DISCOVER SUNSIDE</h2>
+        <h1>MEETINGS & EVENTS</h1>
+        <p>A business meeting can be highly productive. We offer a setting conducive to making crucial decisions, with ample space for when it's time to celebrate successes. The sequence of events is entirely your choice.</p>
+      </div>
+    </div>
+  )
+}

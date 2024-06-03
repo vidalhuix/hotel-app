c1418d7558656b5a2275fad8fabc382bf0984929
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-06-03 15:24:33 +0200

    Login and register page created

---
+++ frontend/src/components/User/Login.jsx
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+const Container = styled.div`
+  background-color: #44564c;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 85px;
+  width: auto;
+  align-items: center;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 1rem;
+  display: grid;
+  grid-template-columns: 95px auto;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  margin-bottom: 0.25rem;
+  margin-top: 0.25rem;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  padding: 0.5rem;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  padding-right: 30px;
+`;
+
+const EyeIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
+  font-size: 1em;
+  color: #ccc;
+`;
+
+const Button = styled.button`
+  margin-top: 0.5rem;
+  padding: 10px 30px;
+  border-radius: 20px;
+  border: none;
+  background-color: #d3af97;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+
+  &:hover {
+    color: #5a675f;
+    background-color: #fff;
+    transform: scale(1.05);
+    transition: all ease 0.3s;
+  }
+`;
+
+const SmallText = styled.p`
+  margin-top: 0.5rem;
+  font-size: 0.875rem;
+  text-align: center;
+`;
+
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
+const SignUpLink = styled(Link)`
+  font-weight: normal;
+  color: white;
+  text-decoration: none;
+  margin-left: 5px;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    width: 0;
+    height: 2px;
+    bottom: -3px;
+    background-color: #d3af97;
+    transition: width 250ms ease-in;
+  }
+
+  &:hover {
+    color: #d3af97;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <h2>LOG IN TO YOUR ACCOUNT</h2>
+        <FormGroup>
+          <Label htmlFor="email">Email:</Label>
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email address"
+            maxLength={254}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password:</Label>
+          <InputWrapper>
+            <Input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter your password"
+              maxLength={20}
+            />
+            <EyeIcon
+              icon={showPassword ? faEye : faEyeSlash}
+              onClick={togglePasswordVisibility}
+            />
+          </InputWrapper>
+        </FormGroup>
+        <Button type="submit">Log In</Button>
+        <SmallText>
+          <BoldText>Don't have an account?</BoldText>
+          <SignUpLink to="/register">Sign up</SignUpLink> now!
+        </SmallText>
+      </Form>
+    </Container>
+  );
+};

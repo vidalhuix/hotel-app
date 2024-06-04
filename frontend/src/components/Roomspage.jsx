2039de772f313d2ea73f3531c6bd082975503b0f
jingh999 <jinghuang0628@gmail.com>
2024-06-04 11:47:49 +0200

    clear files and create hotelrooms page

--- frontend/src/components/Roomspage.jsx
+++ frontend/src/components/Roomspage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #44564c;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
 
 export const Roomspage = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <h1>Hello, Roomspage test!</h1>
+    </Container>
   )
 }

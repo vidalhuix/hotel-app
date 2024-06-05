ceb5299b46a4b47b136b00519d25dcdb3ef442c1
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-06-05 11:51:27 +0200

    Log in, register, delete user added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,40 @@
 import express from "express";
 import cors from "cors";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
 import expressListEndpoints from "express-list-endpoints";
 import roomsData from "./data/hotel-room.json";
 import roomStatusData from "./data/room-status.json";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/flowershop"
-mongoose.connect(mongoUrl)
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/flowershop";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
 
+// User model
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+// Hotelroom model
 const Hotelrooms = mongoose.model("Hotelrooms", {
   id: Number,
   type: String,
@@ -22,17 +45,20 @@ const Hotelrooms = mongoose.model("Hotelrooms", {
   description: String,
   image: String,
   facilities: [String],
-  availability: [{
-    startDate: Date,
-    endDate: Date,
-    availableRooms: Number
-  }]
+  availability: [
+    {
+      startDate: Date,
+      endDate: Date,
+      availableRooms: Number,
+    },
+  ],
 });
 
+// Room status model
 const RoomStatus = mongoose.model("Roomstatus", {
   roomId: Number,
   date: Date,
-  status: Number
+  status: Number,
 });
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -45,14 +71,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware to authenticate user with access token
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  if (!accessToken) {
+    return res
+      .status(401)
+      .json({ error: "Unauthorized: Access token is missing" });
+  }
+
+  const user = await User.findOne({ accessToken });
+  if (!user) {
+    return res
+      .status(401)
+      .json({ error: "Unauthorized: Access token is invalid" });
+  }
+
+  req.user = user;
+  next();
+};
+
 if (+process.env.RESET_DB) {
   const seedDatabase = async () => {
     try {
       await Hotelrooms.deleteMany({});
       await RoomStatus.deleteMany({});
       console.log("Database cleared");
-      const saveRoomPromises = roomsData.map(room => new Hotelrooms(room).save());
-      const saveStatusPromises = roomStatusData.map(status => new RoomStatus(status).save());
+      const saveRoomPromises = roomsData.map((room) =>
+        new Hotelrooms(room).save()
+      );
+      const saveStatusPromises = roomStatusData.map((status) =>
+        new RoomStatus(status).save()
+      );
 
       await Promise.all(saveRoomPromises, saveStatusPromises);
       console.log("Database seeded with initial data");
@@ -66,22 +116,22 @@ if (+process.env.RESET_DB) {
   //     const startDate = new Date(startDateStr);
   //     const endDate = new Date(endDateStr);
   //     const dateArray = [];
-  
+
   //     for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
   //         dateArray.push(new Date(date));  // Ensure each date is a new instance
   //     }
-  
+
   //     return dateArray;
   //   }
-    
+
   //   // Generate dates from 2024-06-01 to 2025-06-01
   //   const startDate = "2024-06-05";
   //   const endDate = "2025-06-05";
   //   const dates = generateDateArray(startDate, endDate);
-    
+
   //   // Array to hold the room and date combinations
   //   const roomDateArray = [];
-    
+
   //   // Loop over roomIds
   //   for (let roomId = 1; roomId <= 7; roomId++) {
   //       // Loop over dates
@@ -91,10 +141,10 @@ if (+process.env.RESET_DB) {
   //   }
 
   //   const fs = require('fs');
-    
+
   //   // Convert roomDateArray to a JSON string
   //   const jsonContent = JSON.stringify(roomDateArray, null, 2);
-    
+
   //   // Write JSON string to a file
   //   fs.writeFile('data/room-status.json', jsonContent, (err) => {
   //       if (err) {
@@ -122,31 +172,31 @@ app.use((req, res, next) => {
 // http://localhost:8080/
 app.get("/", (req, res) => {
   const endpoints = expressListEndpoints(app);
-  res.json(endpoints)
+  res.json(endpoints);
 });
 
 app.get("/hotelrooms", async (req, res) => {
-  try{
+  try {
     const hotelrooms = await Hotelrooms.find();
     res.json(hotelrooms);
-    } catch (error) {
-      res.status(400).send("Rooms cannot be found.")
-    }
+  } catch (error) {
+    res.status(400).send("Rooms cannot be found.");
+  }
 });
 
 app.get("/hotelrooms/:guestamount", async (req, res) => {
   try {
     const amount = parseInt(req.params.guestamount);
     const suitableRooms = await Hotelrooms.find({ capacity: { $gte: amount } });
-    
+
     if (suitableRooms.length > 0) {
       res.json(suitableRooms);
     } else {
-      return res.status(404).json({ error: 'No rooms found' });
+      return res.status(404).json({ error: "No rooms found" });
     }
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
@@ -186,19 +236,123 @@ app.get("/hotelrooms/status/date/:date", async (req, res) => {
 
     const availableRooms = await RoomStatus.find({
       $and: [
-        {"date": { $eq: date }},
+        { date: { $eq: date } },
         //{"status": { $eq: 1 }}
-      ]
+      ],
     });
 
     if (availableRooms.length > 0) {
       res.json(availableRooms);
     } else {
-      res.status(404).json({ error: 'No rooms found' });
+      res.status(404).json({ error: "No rooms found" });
     }
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Registration endpoint to create a new user
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Validate the input
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+    if (!email) {
+      return res.status(400).json({ error: "Email is required" });
+    }
+    if (!password) {
+      return res.status(400).json({ error: "Password is required" });
+    }
+
+    // Check for existing user
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password);
+
+    // Create new user
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    console.error("User creation failed:", err);
+
+    if (err.name === "ValidationError") {
+      return res
+        .status(400)
+        .json({ error: "Validation error", details: err.message });
+    } else if (err.code === 11000) {
+      return res.status(400).json({
+        error: "Duplicate key error",
+        details: "Email or name already exists",
+      });
+    } else {
+      return res
+        .status(500)
+        .json({ error: "Internal server error", details: err.message });
+    }
+  }
+});
+
+// Login endpoint to authenticate a user
+// Find the user by email, if user is found and password is correct, generate and return an access token
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const accessToken = user.accessToken;
+  res.status(200).json({ accessToken });
+});
+
+// Fetch user details, including user ID (needed to delete user)
+app.get("/user-details", authenticateUser, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId).select("name email");
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({
+      message: `Hi ${user.name}, welcome!`,
+      user: {
+        _id: user._id, 
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Delete user endpoint
+app.delete("/users/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 

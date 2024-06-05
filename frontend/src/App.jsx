dd5b63d97069f60ff0e88f4fa5dae74113b6eee9
Your Name <you@example.com>
2024-06-05 10:49:21 +0200

    removed RoomDetails component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Nav } from "./components/Header/Nav";
 import { Login } from "./components/User/Login";
 import { Register } from "./components/User/Register";
 import { Roomspage } from "./components/Roomspage";
-import { RoomDetails } from "./components/RoomDetails";
 import { Hero } from "./components/Header/Hero";
 import { BookingSection } from "./components/Booking/BookingSection";
 import { MeetingSection } from "./components/MeetingSection/MeetingSection";
@@ -23,7 +22,6 @@ export const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/hotelrooms" element={<Roomspage />} />
-        <Route path="/hotelrooms/:type" element={<RoomDetails />} />
       </Routes>
     </Router>
   );

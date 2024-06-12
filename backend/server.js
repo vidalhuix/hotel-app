44b5ffb4fca3f4f024bee724fcb5b48c9e44a43f
jingh999 <jinghuang0628@gmail.com>
2024-06-12 22:56:24 +0200

    Authenticate user for get user bookings endpoint

--- backend/server.js
+++ backend/server.js
@@ -430,9 +430,9 @@ app.post('/booking', async (req, res) => {
 });
 
 //Endpoint to get user's booking information
-app.get('/users/:userId/bookings', async (req, res) => {
+app.get('/user-bookings', authenticateUser, async (req, res) => {
   try {
-    const userId = req.params.userId;
+    const userId = req.user._id;
 
     const user = await User.findById(userId);
     if (!user) {
